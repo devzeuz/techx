293f5ca501f9ba2a50da1dc7a8d7ce6ae9bf12c8
devzeuz <soceeenin@yahoo.com>
2026-01-01 20:40:07 -0800

    cmt2

--- module/lambda/main.tf
+++ module/lambda/main.tf
@@ -1,4 +1,4 @@
-data "archive_file" "zip-lambda"{
+data "archive_file" "zip_lambda"{
     type = "zip"
     source_file = "${path.module}/src/ingestor.py"
     output_path = "${path.module}/src/ingestor.zip"
